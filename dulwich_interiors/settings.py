ec664abd471e99f07abb8467a4e53f16fb04a2cb
EimearCunningham <eimearcunningham@hotmail.com>
2021-07-06 19:38:19 +0000

    Add basic checkout template

--- dulwich_interiors/settings.py
+++ dulwich_interiors/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +62,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'dulwich_interiors.urls'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
@@ -77,6 +83,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
