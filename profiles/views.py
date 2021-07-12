a8cd8e8b284af6cf8a0a8f2b1a50781694398521
EimearCunningham <eimearcunningham@hotmail.com>
2021-07-12 18:36:43 +0000

    Add order history table to profile.html

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,13 @@ from .forms import UserProfileForm
 def profile(request):
     """ Display a user profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully!')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
