bbd7af707aec5a0f9fc38f4baacfb79569f7e06d
EimearCunningham <eimearcunningham@hotmail.com>
2021-07-14 06:20:11 +0000

    Add post handler for add_product view

--- products/views.py
+++ products/views.py
@@ -55,7 +55,16 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ View for store owner to add product """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
