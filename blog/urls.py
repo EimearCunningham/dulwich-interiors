1a68c9a89aaca7e9df7354eae5c3971fcd9f8523
EimearCunningham <eimearcunningham@hotmail.com>
2021-07-15 06:55:23 +0000

    Add functionnality for superuser to add blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('add/', views.add_post, name='add_post'),
     path('', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('add/', views.add_post, name='add_post'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,38 @@
 from django.views import generic
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .models import Post
 from .forms import BlogForm
 
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog.html'
+    template_name = 'blog/blog.html'
 
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
 
 
 def add_post(request):
-    """ Add a post to the blog """
-    form = BlogForm()
+    """ View for store owner to add post """
+    
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added post!')
+            return redirect(reverse('add_post'))
+        else:
+            messages.error(request, 'Failed to add post. Please ensure the form is valid.')
+    else:
+        form = BlogForm()
+
     template = 'blog/add_post.html'
     context = {
         'form': form,
     }
 
     return render(request, template, context)
+
