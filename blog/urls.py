c6d5a4197b79d22e0678041b3223dc42dbcfc319
EimearCunningham <eimearcunningham@hotmail.com>
2021-07-17 18:14:56 +0000

    Update post_detail template to include comments and comment form

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('add/', views.add_post, name='add_post'),
     path('delete/<slug:slug>/', views.delete_post, name='delete_post'),
     path('', views.PostList.as_view(), name='blog'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
     
 ]
